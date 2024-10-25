52d729d4e8e1721563946c46a31673bec2115f59
VovaDev777 <polvovater@gmail.com>
2024-10-25 15:00:12 +0200

    adaptive

---
+++ scripts/mobile-menu.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openMobBtn: document.querySelector("[data-mob-open]"),
+      closeMobBtn: document.querySelector("[data-mob-close]"),
+      mob: document.querySelector("[data-mob]"),
+    };
+  
+    refs.openMobBtn.addEventListener("click", toggleMob);
+    refs.closeMobBtn.addEventListener("click", toggleMob);
+  
+    function toggleMob() {
+      refs.mob.classList.toggle("is-hidden");
+    }
+  })();
